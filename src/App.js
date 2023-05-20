a7482b2f8197259df888830e4a1a47dac5e53de7
Jerry Wan <craftersol728@gmail.com>
2023-05-19 21:22:02 -0700

    added logo path to /

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react'
+import NavBar from './components/navBar.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>hahahha</h1>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path ='/' exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
