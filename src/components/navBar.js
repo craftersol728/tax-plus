05a92fd4924ef34bd7d19962aa919624d99af83a
Jerry Wan <craftersol728@gmail.com>
2023-05-19 21:45:11 -0700

    added icon on the top left

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,14 +1,21 @@
 import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../../src/icon/TaxPlus 2.png' 
+import './navBar.css' // Import your CSS here
+
 
 function NavBar () {
+    const [click , setClick] = useState (false)
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to= "/" className="navbar-logo">
-                        Taxplus
+                    <Link to="/" className="navbar-logo">
+                        <img src={logo} alt="Logo" className="navbar-logo-image" /> {/* Add a class to your logo */}
                     </Link>
+                    <div className='menu-icon'>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
                 </div>
             </nav>
         </>
